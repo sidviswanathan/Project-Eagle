0e9c52c0a11a5b42f784f91c7c811f4f7f650c20
Anush Jayaraman <anushjay@gmail.com>
2011-06-23 00:36:22 -0700

    Adding validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_many :reservations
   belongs_to :course
+
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  validates_format_of :email,
+                      :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
+                      :message => "Invalid email format"
+
 end
