ee6aac2a5d799b2a86d4fcf9dc5209d9d5671b81
Sid Viswanathan <sid@cardmunch.com>
2011-08-03 19:43:00 -0700

    commented out everything

--- app/controllers/listener_controller.rb
+++ app/controllers/listener_controller.rb
@@ -3,24 +3,26 @@ require 'logger'
 class ListenerController < ApplicationController
 
   def index
-    @params = params
+    logger.info params
+    # @params = params
     #Make sure request is a post
-    @inbound_email = InboundEmail.new(:text => params["text"],
-                                      :html => params["html"],
-                                      :to => clean_field(params["to"]),
-                                      :from => clean_field(params["from"]),
-                                      :subject => clean_field(params["subject"])
-                                      )           
-    respond_to do |format|                               
-      if @inbound_email && request.post?
-        @inbound_email.process_incoming_email
-        flash[:notice] = 'Item was successfully created'
-        format.xml { render :xml => @inbound_email, :status => :created}
-      else
-        flash[:notice] = 'Error saving the item'
-        format.xml { render :xml => @inbound_email.errors, :status => :unprocessable_entity}
-      end  
-    end   
+    # @inbound_email = InboundEmail.new(:text => params["text"],
+    #                                       :html => params["html"],
+    #                                       :to => clean_field(params["to"]),
+    #                                       :from => clean_field(params["from"]),
+    #                                       :subject => clean_field(params["subject"])
+    #                                       )           
+    #     respond_to do |format|                               
+    #       if @inbound_email && request.post?
+    #         @inbound_email.process_incoming_email
+    #         flash[:notice] = 'Item was successfully created'
+    #         format.xml { render :xml => @inbound_email, :status => :created}
+    #       else
+    #         flash[:notice] = 'Error saving the item'
+    #         format.xml { render :xml => @inbound_email.errors, :status => :unprocessable_entity}
+    #       end  
+    #     end 
+    render :nothing => true
   end  
   
   private
