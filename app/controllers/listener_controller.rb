42c4c71e06e93676c2cb328ed99cf87e54fb5c16
Sid Viswanathan <sid@cardmunch.com>
2011-08-04 23:51:27 -0700

    added parse email logger statements

--- app/controllers/listener_controller.rb
+++ app/controllers/listener_controller.rb
@@ -6,13 +6,19 @@ class ListenerController < ApplicationController
   
   def index
     
-    logger.info '!!!!!!!!!!!!!!!!!!!!!!!!!!!!' if params["subject"] == 'Reservation Confirmation - Deep Cliff Golf Course'
-    logger.info 'THE TEE DATE IS: '+params["text"].split("Tee Date:")[1].split("Tee Time:")[0].split(", ")[1]
-    logger.info 'THE NUM GOLFERS IS IS: '+params["text"].split("Number of Players:")[1][1..1]
-    time = params["text"].split("Tee Time:")[1].split("Number of Players:")[0]
-    logger.info 'THE TEE TIME IS: '+time[1..time.length-2]
-    logger.info Chronic.parse("August 30").strftime('%Y-%m-%d')
-    logger.info 'DONE WITH ALL PRINT STATEMENTS!!'
+    config = YAML::load(File.read(Rails.root.join('config/courses.yml')))[ENV["RAILS_ENV"]] || {} 
+    
+    logger.info config.keys
+    
+    if params["subject"] == 'Reservation Confirmation - Deep Cliff Golf Course'
+      logger.info 'THE TEE DATE IS: '+Chronic.parse(params["text"].split("Tee Date:")[1].split("Tee Time:")[0].split(", ")[1])
+      logger.info 'THE NUM GOLFERS IS IS: '+params["text"].split("Number of Players:")[1][1..1]
+      time = params["text"].split("Tee Time:")[1].split("Number of Players:")[0]
+      logger.info 'THE TEE TIME IS: '+time[1..time.length-2]
+      logger.info 'DONE WITH ALL PRINT STATEMENTS!!'
+    else
+      #Send Admin Email if Subject not recognized  
+    end
     
     #Reservation.create(:)
     #Reservation(id: integer, date: date, tee_slot: string, availability: boolean, golfers: integer, user_id: integer, course_id: integer, created_at: datetime, updated_at: datetime)
