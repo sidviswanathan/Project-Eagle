0761249defa2315ab772628e18ee9eefb7f77a36
Sid Viswanathan <sid@cardmunch.com>
2011-11-08 23:10:24 -0800

    added new API endpoint logic for login in Device_communication.rb and User.rb

--- app/controllers/device_communication_controller.rb
+++ app/controllers/device_communication_controller.rb
@@ -2,6 +2,10 @@ class DeviceCommunicationController < ApplicationController
   
   # These are the clinet API endpoints for all devices communicating witht the Prestee server
   # Below is the expected format for paramters received from all client devices
+  
+  # ==========================================
+  # = DEFINE STANDARD PARAMETER FORMATS ======
+  # ==========================================
 
   # course_id   => 1                  (Integer)   As defined in the Course model
   # num_golfers => 2                  (Integer    Range between 2-4
@@ -11,15 +15,44 @@ class DeviceCommunicationController < ApplicationController
   # lName       => 'last_name'        (String)
   # email       => 'name@domain.com'  (String)
   
+  # ==========================================
+  # = DEFINE STANDARD RESPONSE OBJECT FORMAT =
+  # ==========================================
+
+  # render :json => {:status=>'success or failure',
+  #                  :statusCode => 2XX,4XX,5XX,
+  #                  :message=>'Insert comment here',
+  #                  :response=>"insert response object here"
+  #                 }
+  
+  RESPONSE_OBJECT              = {}
+  RESPONSE_OBJECT[:status]     = "failure"
+  RESPONSE_OBJECT[:statusCode] = 500
+  RESPONSE_OBJECT[:response]   = ""
+  RESPONSE_OBJECT[:message]    = "The server encountered an unexpected condition which prevented it from fulfilling the request"
+  
   # ===================================================================
   # = httpo://presstee.com/device_communication/login =================
   # ===================================================================
   
   def login
     # Receive all params from post request, store each one individually
-    # Make call to User model
-    response = login()
-    return response
+    user = User.login(fName, lName, email)
+    if user
+      reservation = Reservation.book_tee_time(user, course_id, num_golfers, time, date)
+      if reservation
+        RESPONSE_OBJECT[:status]     = "success"
+        RESPONSE_OBJECT[:statusCode] = 200
+        RESPONSE_OBJECT[:message]    = "The server successfully made the Reservation.book_tee_time() request"
+        return RESPONSE_OBJECT.to_json
+      else
+        RESPONSE_OBJECT[:message] = "The server failed to make the Reservation.book_tee_time() request"
+        return RESPONSE_OBJECT.to_json
+      end  
+    else
+      RESPONSE_OBJECT[:message] = "The server failed to make the User.login() request"
+      return RESPONSE_OBJECT.to_json
+    end    
   end  
   
   # ===================================================================
@@ -40,7 +73,7 @@ class DeviceCommunicationController < ApplicationController
   def book_reservation
     # Receive all params from post request, store each one individually
     # Make call to Reservation model
-    response = Reservation.book_tee_times()
+    reservation = Reservation.book_tee_time(user, course_id, num_golfers, time, date)
     return response
   end
   
@@ -49,11 +82,3 @@ class DeviceCommunicationController < ApplicationController
   
 end
 
-# ==========================================
-# = DEFINE STANDARD RESPONSE OBJECT FORMAT =
-# ==========================================
-
-# render :json => {:status=>'success or failure',
-#                  :message=>'Insert comment here',
-#                  :response=>"insert response object here"
-#                 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,25 @@
 class User < ActiveRecord::Base
+  
   has_many :reservations
   belongs_to :course
 
   validates_presence_of :email
   validates_uniqueness_of :email
-  validates_format_of :email,
-                      :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
-                      :message => "Invalid email format"
-
+  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "Invalid email format"
+  
+  # Login method for user, creates a User record
+  # INPUT:  
+  # OUTPUT: 
+  
+  def login()
+    u = User.find_or_create_by_email(fName, lName, email)
+    
+    login_info = {:email=>'sid.viswanathan@gmail.com', :first_name=>'Sid', :last_name=>'Viswanathan'}
+    user = User.find_or_create_by_email(info)
+    
+    # Create or find user record (must build for the case wehre user deletes app and reinstalls it and signs in again)
+    # Return success object or failure object
+    return
+  end
+  
 end
