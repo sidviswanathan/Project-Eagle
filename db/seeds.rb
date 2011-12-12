7a1d553205b29f17d474114c5bcb3ca5d71cce86
Sid Viswanathan <sid@cardmunch.com>
2011-12-11 20:33:05 -0800

    added db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+u = User.find_by_email('carlcwheatey@gmail.com')
+if u == nil
+  u.email = 'carlcwheatey@gmail.com'
+  u.f_name = 'Carl'
+  u.l_name = 'Wheatley'
+  u.device_name = 'iPhone'
+  u.os_version = '5.0'
+  u.app_version = '1.0'
+  
+  if !u.save
+    puts 'user seed failed!'
+  end
+end
