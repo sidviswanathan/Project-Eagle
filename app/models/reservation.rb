db101699cb60ceae8df096bd3f54e5813e3fef75
Arjun Vasan <arjun.vasan@gmail.com>
2011-12-17 12:22:12 -0800

    Testing booking via API simulator

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -65,7 +65,7 @@ class Reservation < ActiveRecord::Base
   #IMPLEMENT: Move this to a separate module file for all Fore API calls.  Every API should have it's own module
   #SAMPLE: response = http.post("http://dump-them.appspot.com/cgi-bin/bk.pl?CourseID=1&Date=2011-12-19&Time=06:08&Email=arjun.vasan@gmail.com&Quantity=2&AffiliateID=029f2fw&Password=eagle", headers)
   
-  def book_time_via_fore_reservations_api(reservation_info)
+  def self.book_time_via_fore_reservations_api(reservation_info)
     
     url = URI.parse(Course::DEEP_CLIFF_API_HOST)
     http = Net::HTTP.new(url.host, url.port)
