2514886ce512e393b54c1cd55bdd95ccfbca9f1d
Arjun Vasan <arjun.vasan@gmail.com>
2011-12-24 18:26:47 -0800

    Changing JSON Structure for Hour Response

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,6 @@ ActionController::Routing::Routes.draw do |map|
   
   map.connect "/listener", :controller => "listener", :action => "index"
   
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  map.connect ':controller/:action/:id', :conditions => { :method => :any }
+  map.connect ':controller/:action/:id.:format' :conditions => { :method => :any }
 end
