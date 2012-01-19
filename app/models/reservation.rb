446b6ddff9a1ce2c2241c7d8aef702aad81a05d6
Arjun Vasan <arjun.vasan@gmail.com>
2012-01-18 19:40:27 -0800

    Testing

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -20,7 +20,8 @@ class Reservation < ActiveRecord::Base
     reservation_info = {:course_id=>course_id, :golfers=>golfers, :time=>time, :date=>date}
     
     # Make the API reservation call here
-    booking = book_time_via_api(reservation_info)
+    u = User.find_by_email(email)
+    booking = book_time_via_api(reservation_info,u)
     if XmlSimple.xml_in(booking.body).has_key?("confirmation")
       confirmation_code = XmlSimple.xml_in(booking.body)["confirmation"][0]
       
@@ -54,13 +55,13 @@ class Reservation < ActiveRecord::Base
   # INPUT: http://dump-them.appspot.com/cgi-bin/bk.pl?CourseID=1&Date=2011-12-19&Time=06:08&Email=arjun.vasan@gmail.com&Quantity=2&AffiliateID=029f2fw&Password=eagle  
   # OUTPUT:  
   
-  def self.book_time_via_api(reservation_info)
+  def self.book_time_via_api(reservation_info,u)
     
     case reservation_info[:course_id]
     
     when Course::DEEP_CLIFF_COURSE_ID
       logger.info "Returning Booking Response"
-      return book_time_via_fore_reservations_api(reservation_info)
+      return book_time_via_fore_reservations_api(reservation_info,u)
     when Course::SOME_OTHER_COURSE_ID 
       # Call function corresponding to the courses API
     else
@@ -73,18 +74,16 @@ class Reservation < ActiveRecord::Base
   #IMPLEMENT: Move this to a separate module file for all Fore API calls.  Every API should have it's own module
   #SAMPLE: response = http.post("http://dump-them.appspot.com/cgi-bin/bk.pl?CourseID=1&Date=2011-12-19&Time=06:08&Email=arjun.vasan@gmail.com&Quantity=2&AffiliateID=029f2fw&Password=eagle", headers)
   
-  def self.book_time_via_fore_reservations_api(reservation_info)
-    
+  def self.book_time_via_fore_reservations_api(reservation_info,u)
+    uri = "#{Course::DEEP_CLIFF_API_URL}?CourseID=#{reservation_info[:course_id]}&Date=#{reservation_info[:date]}&Time=#{reservation_info[:time]}&Email=#{reservation_info[:email]}&FirstName=#{u[:f_name]}&LastName=#{u[:l_name]}&ExpMnth=#{DEFAULT_CC_MONTH}&ExpYear=#{DEFAULT_CC_YEAR}&CreditCard=#{DEFAULT_CC_NUM}&Phone=5628884454&Quantity=#{reservation_info[:golfers]}&AffiliateID=#{Course::DEEP_CLIFF_API_AFFILIATE_ID}&Password=#{Course::DEEP_CLIFF_API_PASSWORD}"
+    puts uri
     url = URI.parse(Course::DEEP_CLIFF_API_HOST)
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
     headers = {}
 
     begin
-      uri = "#{Course::DEEP_CLIFF_API_URL}?CourseID=#{reservation_info[:course_id]}&Date=#{reservation_info[:date]}&Time=#{reservation_info[:time]}&Email=#{reservation_info[:email]}&FirstName=#{reservation_info[:user][:f_name]}&LastName=#{reservation_info[:user][:l_name]}&ExpMnth=#{DEFAULT_CC_MONTH}&ExpYear=#{DEFAULT_CC_YEAR}&CreditCard=#{DEFAULT_CC_NUM}&Phone=5628884454&Quantity=#{reservation_info[:golfers]}&AffiliateID=#{Course::DEEP_CLIFF_API_AFFILIATE_ID}&Password=#{Course::DEEP_CLIFF_API_PASSWORD}"
-      puts uri
       response = http.post(uri, headers)
-      
     rescue
       return nil
     end
