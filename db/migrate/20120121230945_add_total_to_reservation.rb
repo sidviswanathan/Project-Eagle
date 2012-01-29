f03794de8756279b2361f07dfe59aeed60fcd718
Sid Viswanathan <sid@cardmunch.com>
2012-01-28 17:58:50 -0800

    removed migration file

--- db/migrate/20120121230945_add_total_to_reservation.rb
+++
@@ -1,9 +0,0 @@
-class AddTotalToReservation < ActiveRecord::Migration
-  def self.up
-    add_column :reservations, :total, :string
-  end
-
-  def self.down
-    remove_column :reservations, :total
-  end
-end
