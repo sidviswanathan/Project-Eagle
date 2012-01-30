76e435b332b79d26d0f2f7a9c21fe4604f70ea02
Arjun Vasan <arjun.vasan@gmail.com>
2012-01-30 13:43:34 -0800

    Testing SendGrid mail

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,9 +1,9 @@
 class Notifier < ActionMailer::Base
-  default :from => "test@presstee.com"
 
   # send a signup email to the user, pass in the user object that contains the user's email address
   def signup_email(email)
     mail( :to => email, 
+          :from => "test@presstee.com"
           :subject => "Thanks for signing up" )
   end
 end
