cfae80428814365b283fce636929897f3b4a29f8
Arjun Vasan <arjun.vasan@gmail.com>
2012-01-30 13:52:29 -0800

    Testing SendGrid

--- app/models/confirm_mailer.rb
+++ app/models/confirm_mailer.rb
@@ -1,4 +1,9 @@
 class ConfirmMailer < ActionMailer::Base
-  
-
+  def signup_notification(email)
+    recipients "Arjun Vasan <#{email}>"
+    from       "My Forum "
+    subject    "Please activate your new account"
+    sent_on    Time.now
+    body       "Hellow"
+  end
 end
