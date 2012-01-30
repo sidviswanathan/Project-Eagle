222a2dfb3ad162ff74ad5b11563a2716f2d9c07f
Arjun Vasan <arjun.vasan@gmail.com>
2012-01-30 14:03:25 -0800

    Testing SendGrid mail

--- app/models/confirm_mailer.rb
+++ app/models/confirm_mailer.rb
@@ -1,8 +1,8 @@
 class ConfirmMailer < ActionMailer::Base
   def signup_notification(email)
-    recipients "<#{email}>"
-    from       "PressTee booking <confirm@mail.presstee.com>"
-    subject    "Please confirm or cancel your booking"
+    recipients "Arjun Vasan <#{email}>"
+    from       "PressTee booking"
+    subject    "Please activate your new account"
     sent_on    Time.now
     body       "Hellow"
   end
