75a5aaaedfa947f255f05f5ca8d0222f8e757af2
Arjun Vasan <arjun.vasan@gmail.com>
2012-02-01 14:21:52 -0800

    Testing Scheduler

--- app/models/confirm_mailer.rb
+++ app/models/confirm_mailer.rb
@@ -1,10 +1,15 @@
 class ConfirmMailer < ActionMailer::Base
+  SIGNATURE = <<-eos
+      Thank you,
+      Deep Cliff Golf Course (via PressTee)
+      http://www.playdeepcliff.com/
+    eos
   def reminder(data)
     recipients "#{data['f_name']} #{data['l_name']} <#{data['email']}>"
     from       "Deep Cliff Golf Course"
-    subject    "Tee Time Reservation Reminder"
+    subject    data['subject']
     sent_on    Time.now
-    body       "Dear #{data['f_name']} #{data['l_name']}\n\nPlease remember to cancel your reservation if you are not planning on showing up.\n\nThank you, come again, Deep Cliff"
+    body       "Dear #{data['f_name']} #{data['l_name']}\n\n#{data['body']}\n\n#{SIGNATURE}"
   end
 
 end
