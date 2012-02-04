b905d1fca2d805f7881690d13c4e4190d2deef4a
Arjun Vasan <arjun.vasan@gmail.com>
2012-02-04 11:13:17 -0800

    testing mobile web app

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -9,20 +9,9 @@ require 'lib/app/mobile.rb'
 class MobileController < ApplicationController
   before_filter :get_mobile_app
   skip_before_filter :verify_authenticity_token 
-  
 
-  ADD_TASK_HOST                         = 'http://project-eagle.appspot.com'
-  ADD_TASK_URI                          = '/schedule/'
-  
-  @app = nil
-  
-
-  
   def get_mobile_app
     @app = MobileApp.new(params)
   end
-  
-  
-
 
 end
