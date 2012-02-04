2488dcc5012cfb63624193588ab7504b554f6235
Arjun Vasan <arjun.vasan@gmail.com>
2012-02-04 10:14:37 -0800

    testing mobile web app

--- app/controllers/device_communication_controller.rb
+++ app/controllers/device_communication_controller.rb
@@ -4,43 +4,7 @@ require 'apns'
 require 'xmlsimple'
 require 'date'
 require 'lib/api/fore.rb'
-
-class MobileApp
-  attr_accessor :course, :time, :golfers, :date, :params, :d2, :d, :times, :ampm
-  def initialize(params)
-    @course = Course.find(params[:course_id].to_i)
-    if params[:time].nil?
-      params[:time] = ""
-    end
-    if params[:date].nil?
-      params[:date] = Date.today.strftime("%Y-%m-%d")
-    end
-    
-    df = "%A, %B %e"
-    @time = params[:time]
-    @ampm = Time.parse(@time).strftime("%p")
-    @golfers = params[:golfers]
-    @date = params[:date]
-    @date_ob = Date.parse(params[:date])
-    @params = params
-    d = Date.today
-    @d2 = [d.strftime(df),(d+1).strftime(df),(d+2).strftime(df),(d+3).strftime(df),(d+4).strftime(df),(d+5).strftime(df),(d+6).strftime(df)]
-    
-    @d = [d.strftime("%Y-%m-%d"),(d+1).strftime("%Y-%m-%d"),(d+2).strftime("%Y-%m-%d"),(d+3).strftime("%Y-%m-%d"),(d+4).strftime("%Y-%m-%d"),(d+5).strftime("%Y-%m-%d"),(d+6).strftime("%Y-%m-%d")]
-    
-  end
-  def get_query
-    kvs = []
-    @params.each_pair do |k,v|
-      kvs.push(k+"="+v)
-    end
-    return "?#{kvs.join('&')}"
-  end
-  def get_url(action,new_params)
-    @params = @params.merge(new_params)
-    uri = "/device_communication/#{action}#{get_query}"
-  end
-end
+require 'lib/app/mobile.rb'
 
 class DeviceCommunicationController < ApplicationController
   
@@ -120,6 +84,10 @@ class DeviceCommunicationController < ApplicationController
     response_object = intitiate_response_object
     render 'mobile/date'
     
+  end
+  
+  def app_test
+    render 'mobile/test'
   end
   
   def app_times
---
+++ lib/app/mobile.rb
@@ -0,0 +1,43 @@
+require 'pp'
+require 'json'
+require 'apns'
+require 'xmlsimple'
+require 'date'
+require 'lib/api/fore.rb'
+
+class MobileApp
+  attr_accessor :course, :time, :golfers, :date, :params, :d2, :d, :times, :ampm
+  def initialize(params)
+    @course = Course.find(params[:course_id].to_i)
+    if params[:time].nil?
+      params[:time] = ""
+    end
+    if params[:date].nil?
+      params[:date] = Date.today.strftime("%Y-%m-%d")
+    end
+    
+    df = "%A, %B %e"
+    @time = params[:time]
+    @ampm = Time.parse(@time).strftime("%p")
+    @golfers = params[:golfers]
+    @date = params[:date]
+    @date_ob = Date.parse(params[:date])
+    @params = params
+    d = Date.today
+    @d2 = [d.strftime(df),(d+1).strftime(df),(d+2).strftime(df),(d+3).strftime(df),(d+4).strftime(df),(d+5).strftime(df),(d+6).strftime(df)]
+    
+    @d = [d.strftime("%Y-%m-%d"),(d+1).strftime("%Y-%m-%d"),(d+2).strftime("%Y-%m-%d"),(d+3).strftime("%Y-%m-%d"),(d+4).strftime("%Y-%m-%d"),(d+5).strftime("%Y-%m-%d"),(d+6).strftime("%Y-%m-%d")]
+    
+  end
+  def get_query
+    kvs = []
+    @params.each_pair do |k,v|
+      kvs.push(k+"="+v)
+    end
+    return "?#{kvs.join('&')}"
+  end
+  def get_url(action,new_params)
+    @params = @params.merge(new_params)
+    uri = "/device_communication/#{action}#{get_query}"
+  end
+end
