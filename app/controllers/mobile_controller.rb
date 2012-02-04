d729598a57a0d73719f59078cb72ae2bc31ea604
Arjun Vasan <arjun.vasan@gmail.com>
2012-02-04 11:11:40 -0800

    testing mobile web app

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -7,55 +7,22 @@ require 'lib/api/fore.rb'
 require 'lib/app/mobile.rb'
 
 class MobileController < ApplicationController
+  before_filter :get_mobile_app
   skip_before_filter :verify_authenticity_token 
+  
 
   ADD_TASK_HOST                         = 'http://project-eagle.appspot.com'
   ADD_TASK_URI                          = '/schedule/'
   
-  def intitiate_response_object    
-    response_object              = Hash.new
-    response_object[:status]     = "failure"
-    response_object[:statusCode] = 500
-    response_object[:response]   = ""
-    response_object[:message]    = "The server encountered an unexpected condition which prevented it from fulfilling the request"
-    return response_object
-  end
+  @app = nil
   
-  def get_mobile_app(params)
-    return MobileApp.new(params)
+
+  
+  def get_mobile_app
+    @app = MobileApp.new(params)
   end
   
   
-  def index
-    @app = get_mobile_app(params)
-    response_object = intitiate_response_object
-    render 'mobile/index'
-    
-  end
-  
-  def app_num
-    @app = get_mobile_app(params)
-    response_object = intitiate_response_object
-    render 'mobile/num'
-    
-  end
-  
-  def app_date
-    @app = get_mobile_app(params)
-    response_object = intitiate_response_object
-    render 'mobile/date'
-    
-  end
-  
-  def app_test
-    render 'mobile/test'
-  end
-  
-  def app_times
-    @app = get_mobile_app(params)
-    response_object = intitiate_response_object
-    render 'mobile/time'
-    
-  end
+
 
 end
