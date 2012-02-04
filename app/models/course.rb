7ca78f76afc016bc4f5dee5376fb784d5720b67b
Arjun Vasan <arjun.vasan@gmail.com>
2012-02-04 11:12:42 -0800

    testing mobile web app

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,11 +1,4 @@
 class Course < ActiveRecord::Base  
   has_many :users
-  has_many :reservations
-  
-  def self.get_available_times(course,dater)  
-    dates = JSON.parse(course.available_times)
-    return dates[dater]["day"]
-  end
-
-  
+  has_many :reservations  
 end
