bae73b1c9575110445d40d2b5f9bb4704ad58b55
Arjun Vasan <arjun.vasan@gmail.com>
2012-02-15 12:43:13 -0800

    test

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -4,7 +4,6 @@ require 'apns'
 require 'xmlsimple'
 require 'date'
 require 'lib/api/fore.rb'
-require 'gdata'
 
 class CourseController < ApplicationController
   skip_before_filter :verify_authenticity_token 
