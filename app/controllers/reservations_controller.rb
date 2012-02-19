658209afa19cdaa594b9ffabd84f717cb94513a7
Arjun Vasan <arjun.vasan@gmail.com>
2012-02-18 17:51:57 -0800

    fixing json date bug

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -110,7 +110,7 @@ class ReservationsController < ApplicationController
       book_dt = rr.created_at.in_time_zone("Pacific Time (US & Canada)")
       teetime = rr.date.strftime("%Y-%m-%d")+" "+rr.time
       tt_dt = DateTime.strptime(teetime,"%Y-%m-%d %H:%M")
-      data["early"].push(book_dt-tt_dt)
+      data["early"].push({:dt=>book_dt-tt_dt,:book=>book_dt,:teetime=>tt_dt)
       data["cursor"] = rr.id.to_s
     end
     render :json => data.to_json
