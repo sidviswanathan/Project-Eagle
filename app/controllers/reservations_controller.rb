f6660160fcadd4891482323d200c6115358a7f46
Arjun Vasan <arjun.vasan@gmail.com>
2012-02-25 18:37:50 -0800

    Updating Test Data

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -106,12 +106,12 @@ class ReservationsController < ApplicationController
   def test_data
     r = Reservation.all(:conditions=>["course_id='2'"])
     data = {"early"=>[],"cursor"=>0}
-    data_bar = [["0"],["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"]]
+    data_bar = [["0",0],["1",0],["2",0],["3",0],["4",0],["5",0],["6",0],["7",0],["8",0]]
     r.each do |rr|
       book_dt = rr.created_at.in_time_zone("Pacific Time (US & Canada)")
       teetime = rr.date.strftime("%Y-%m-%d")+" "+rr.time+" PST"
       tt_dt = DateTime.strptime(teetime,"%Y-%m-%d %H:%M %Z")
-      data_bar[((book_dt-tt_dt)/86400).abs].push((book_dt-tt_dt).abs)
+      data_bar[((book_dt-tt_dt)/86400).abs][1]+=1
         
       data["early"].push({:dt=>book_dt-tt_dt,:book=>book_dt,:teetime=>tt_dt})
       data["cursor"] = rr.id.to_s
