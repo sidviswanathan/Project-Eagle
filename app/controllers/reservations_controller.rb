e4733b1253c4a449b6b9a349733a441c405fe6e9
Arjun Vasan <arjun.vasan@gmail.com>
2012-02-29 15:28:51 -0800

    Adding DataStore and Cursored Queries

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -110,7 +110,8 @@ class ReservationsController < ApplicationController
       data_bar = [["0",0,0,0,0,0,0,0,0],["1",0,0,0,0,0,0,0,0],["2",0,0,0,0,0,0,0,0],["3",0,0,0,0,0,0,0,0],["4",0,0,0,0,0,0,0,0],["5",0,0,0,0,0,0,0,0],["6",0,0,0,0,0,0,0,0],["7",0,0,0,0,0,0,0,0],["8",0,0,0,0,0,0,0,0],
                   ["9",0,0,0,0,0,0,0,0],["10",0,0,0,0,0,0,0,0],["11",0,0,0,0,0,0,0,0],["12",0,0,0,0,0,0,0,0],["13",0,0,0,0,0,0,0,0],["14",0,0,0,0,0,0,0,0],["15",0,0,0,0,0,0,0,0],["16",0,0,0,0,0,0,0,0],
                   ["17",0,0,0,0,0,0,0,0],["18",0,0,0,0,0,0,0,0],["19",0,0,0,0,0,0,0,0],["20",0,0,0,0,0,0,0,0],["21",0,0,0,0,0,0,0,0],["22",0,0,0,0,0,0,0,0],["23",0,0,0,0,0,0,0,0]]
-      d = DataStore.create(:data=>{"cursor"=>0,"early"=>[],"data_bar"=>data_bar}.to_json,:course_id=>@course_id.to_i)
+
+      d = DataStore.create(:data=>{"cursor"=>0,"early"=>[],"json"=>[],"data_bar"=>data_bar}.to_json,:course_id=>@course_id.to_i)
     end
       
     data = JSON.parse(d.data)
@@ -127,8 +128,9 @@ class ReservationsController < ApplicationController
 
         data["early"].push({:dt=>book_dt-tt_dt,:book=>book_dt,:teetime=>tt_dt})
         data["cursor"] = rr.id
+       
       end
-
+      data["json"] = data["json"] + JSON.parse(r.to_json)
       d.data = data.to_json
       d.save
       
