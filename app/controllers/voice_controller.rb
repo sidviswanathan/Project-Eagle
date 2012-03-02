878078709b9c422b05ac0411cd1dad82ab538e07
Arjun Vasan <arjun.vasan@gmail.com>
2012-03-01 17:58:32 -0800

    Voice Answering

--- app/controllers/voice_controller.rb
+++ app/controllers/voice_controller.rb
@@ -12,10 +12,22 @@ class VoiceController < ApplicationController
   def recieve
     response = Twilio::TwiML::Response.new do |r|
       greeting = 'Welcome to Deep Cliff Golf Course.  To book a Tee Time, press 1.  To speak with the course, press 2'
-      r.Gather do |d|
+      r.Gather :action => "/voice/options" do |d|
         d.Say greeting, :voice => 'man'
       end
 
+    end
+    render :text => response.text
+  end
+  
+  def options
+    response = Twilio::TwiML::Response.new do |r|
+      greeting = 'Welcome to Deep Cliff Golf Course.  To book a Tee Time, press 1.  To speak with the course, press 2'
+      if params[:Digits] == "1"
+        r.Say "Good Choice, you just won a million dollars!", :voice => 'man'
+      else
+        r.Say "Too bad, you would have won a million dollars if you booked a tee time", :voice => 'woman'
+      end
     end
     render :text => response.text
   end
