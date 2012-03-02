e112fe70f492c25d644bb2fb0fdeacd906759bf1
Arjun Vasan <arjun.vasan@gmail.com>
2012-03-01 18:59:48 -0800

    Voice Answering

--- app/controllers/voice_controller.rb
+++ app/controllers/voice_controller.rb
@@ -49,8 +49,8 @@ class VoiceController < ApplicationController
     data = JSON.parse(d.data)
 
     response = Twilio::TwiML::Response.new do |r|
-      r.Say "Please wait about a minute while we process your request", :voice => 'man'
-      r.Pause :length=>90
+      r.Say "Please wait about 30 seconds while we process your request", :voice => 'man'
+      r.Pause :length=>30
       r.Redirect "/voice/gettime"
     end
     render :text => response.text
