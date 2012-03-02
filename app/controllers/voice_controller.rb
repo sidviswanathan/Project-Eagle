0844ad649c2f866aa905176e457464a1cc56bf1f
Arjun Vasan <arjun.vasan@gmail.com>
2012-03-01 17:51:54 -0800

    Voice Answering

--- app/controllers/voice_controller.rb
+++ app/controllers/voice_controller.rb
@@ -11,7 +11,11 @@ class VoiceController < ApplicationController
   skip_before_filter :verify_authenticity_token
   def recieve
     response = Twilio::TwiML::Response.new do |r|
-      r.Say 'Welcome to Deep Cliff Golf Course', :voice => 'man'
+      greeting = 'Welcome to Deep Cliff Golf Course.  To book a Tee Time, press 1.  To speak with the course, press 2'
+      r.Gather do |d|
+        d.Say greeting, :voice => 'man'
+      end
+
     end
     render :text => response.text
   end
