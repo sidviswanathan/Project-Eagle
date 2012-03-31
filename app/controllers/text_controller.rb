7e23a5d2e85666ed47c5d7053852be8132728120
Arjun Vasan <arjun.vasan@gmail.com>
2012-03-31 16:20:51 -0700

    Changing header text to Deep Cliff Golf Course

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -16,55 +16,83 @@ class TextController < ApplicationController
     course = Course.find(params[:course_id].to_s)
     phone = params[:From].sub("+1","")
     body = params[:Body].downcase
-    uname = ""
-    user = Customer.find_or_create_by_phone(phone)
-    if !user.f_name.nil?
-      uname = user.f_name
-    end
-    
-    booking = parse_booking(body)
-    if !booking.nil?
+    if body.length == 1
+      @client = Twilio::REST::Client.new T_SID, T_TOKEN
       d = DataStore.find_by_name("sms_"+params[:From])
-      if !d.nil?
-        cdate = booking[:date].strftime("%Y-%m-%d")
-        ctime = booking[:time].strftime("%l:%p")
-        d.update_attributes :data => {"course"=>course.id,"text"=>"","date"=>"#{cdate}","time"=>"#{ctime}","golfers"=>"#{booking[:golfers]}"}.to_json
+      if body == '0'
+        @client.account.sms.messages.create(
+          :from => '+14087035664',
+          :to => params[:From],
+          :body => "Sorry you weren't able to complete your booking.  Please call us if you'd still like to play golf at Deep Cliff!"
+        )
       else
-        booking_info = {:name=>"sms_"+params[:From],:data=>{"course"=>course.id,"text"=>"","date"=>"#{cdate}","time"=>"#{ctime}","golfers"=>"#{booking[:golfers]}"}.to_json}
-        d = DataStore.create(booking_info)
+        @client.account.sms.messages.create(
+          :from => '+14087035664',
+          :to => params[:From],
+          :body => "Your tee time has been confirmed, thanks for your business!"
+        )
       end
       
-      clean_date = booking[:date].strftime("%A %B %d")
-      clean_time = booking[:time].strftime("%l:%M%p")
       
-      
-      
-      closest,avail = get_slots(course,cdate,ctime)
-      if avail.length > 0
-        clean_time = Time.parse(closest).strftime("%l:%M%p")
-        slot_list = ", OR "
-        
-        avail.each_with_index do |ss,ii|
-          slot_list +=  "#{(ii+2).to_s} for #{Time.parse(ss).strftime('%l:%M%p')}, "
-        end
-        sms = "#{booking[:golfers]} golfers on #{clean_date} at #{clean_time}?  Reply 1 to confirm #{slot_list}."
-        
-        
-      else
-        sms = "Sorry, there don't seem to be any available slots for around #{clean_time} on #{clean_date}.  Please try for a different date/time.  "
-      end 
-      
-      
-    else
-      sms = "Sorry we didn't quite get that!  Text something like 'for 4 golfers on sunday at 10', or call us at 408-703-5664. Thanks!"
-    end
+    elsif body.length > 1
+      uname = ""
+      user = Customer.find_or_create_by_phone(phone)
+      if !user.f_name.nil?
+        uname = user.f_name
+      end
 
-    @client = Twilio::REST::Client.new T_SID, T_TOKEN
-    @client.account.sms.messages.create(
-      :from => '+14087035664',
-      :to => params[:From],
-      :body => sms
-    )
+      booking = parse_booking(body)
+      if !booking.nil?
+        d = DataStore.find_by_name("sms_"+params[:From])
+        if !d.nil?
+          cdate = booking[:date].strftime("%Y-%m-%d")
+          ctime = booking[:time].strftime("%l:%p")
+          d.update_attributes :data => {"course"=>course.id,"text"=>"","date"=>"#{cdate}","time"=>"#{ctime}","golfers"=>"#{booking[:golfers]}"}.to_json
+        else
+          booking_info = {:name=>"sms_"+params[:From],:data=>{"course"=>course.id,"text"=>"","date"=>"#{cdate}","time"=>"#{ctime}","golfers"=>"#{booking[:golfers]}"}.to_json}
+          d = DataStore.create(booking_info)
+        end
+
+        clean_date = booking[:date].strftime("%A %B %d")
+        clean_time = booking[:time].strftime("%l:%M%p")
+
+
+
+        closest,avail = get_slots(course,cdate,ctime)
+        if avail.length > 0
+          clean_time = Time.parse(closest).strftime("%l:%M%p")
+          slot_list = ", OR "
+
+          avail.each_with_index do |ss,ii|
+            slot_list +=  "#{(ii+2).to_s} for #{Time.parse(ss).strftime('%l:%M%p')}, "
+          end
+          sms = "#{booking[:golfers]} golfers on #{clean_date} at #{clean_time}?  Reply 1 to confirm #{slot_list} OR 0 to cancel."
+
+
+        else
+          sms = "Sorry, there don't seem to be any available slots for around #{clean_time} on #{clean_date}.  Please try for a different date/time.  "
+        end 
+
+
+      else
+        sms = "Sorry we didn't quite get that!  Text something like 'for 4 golfers on sunday at 10', or call us at 408-703-5664. Thanks!"
+      end
+
+      @client = Twilio::REST::Client.new T_SID, T_TOKEN
+      @client.account.sms.messages.create(
+        :from => '+14087035664',
+        :to => params[:From],
+        :body => sms
+      )
+    else
+      @client = Twilio::REST::Client.new T_SID, T_TOKEN
+      @client.account.sms.messages.create(
+        :from => '+14087035664',
+        :to => params[:From],
+        :body => "Text 14087035664 something like 'Sunday at 3pm for 2 golfers' to book a tee time at Deep Cliff."
+      )
+    end
+    
 
     render :nothing => true
   end
