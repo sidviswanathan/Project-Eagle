d0b9448e6e5c79d9ee8c4bac181b2d4967f1a4f8
Arjun Vasan <arjun.vasan@gmail.com>
2012-03-31 16:14:06 -0700

    Changing header text to Deep Cliff Golf Course

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -45,7 +45,7 @@ class TextController < ApplicationController
         slot_list = ", OR "
         
         avail.each_with_index do |ss,ii|
-          slot_list += (ii+2).to_s +" for "+Time.parse(ss).strftime("%l:%M%p")
+          slot_list +=  "#{(ii+2).to_s} for #{Time.parse(ss).strftime('%l:%M%p')}"
         end
         sms = "#{booking[:golfers]} golfers on #{clean_date} at #{clean_time}?  Reply 1 to confirm #{slot_list}."
         
