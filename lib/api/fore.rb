7ea6a08f66d325fc2fd1f692586d2642351ac1d9
Sid Viswanathan <sid@cardmunch.com>
2012-05-30 22:49:22 -0700

    debugging keys nil issues in fore.rb

--- lib/api/fore.rb
+++ lib/api/fore.rb
@@ -93,6 +93,8 @@ module Fore
       
       begin
         response = http.get(query, headers)
+        puts "The vallue of of response in the self.update method is:"
+        puts response
         dat = response.body.gsub("\n","").split("<avail>")[1]
         if dat.index("teetime").nil?:
   				response = ""
