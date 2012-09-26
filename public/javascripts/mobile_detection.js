bee32d48fe5c1cd99750825f21e7ff409dfc88ea
Sid Viswanathan <sid@cardmunch.com>
2012-09-25 22:52:14 -0700

    Testing mobile detection code

---
+++ public/javascripts/mobile_detection.js
@@ -0,0 +1,19 @@
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i) ? true : false;
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i) ? true : false;
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) ? true : false;
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
+    }
+};
+
+if( isMobile.any() ) window.location = "http://www.google.com/";
