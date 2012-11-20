9dd4156a9076ac97cac18a501ef445895d8b9772
Sid Viswanathan <sid@cardmunch.com>
2012-11-19 21:50:01 -0800

    updated discounted_pricing column to be text

--- db/migrate/20121119062707_add_discounted_pricing.rb
+++ db/migrate/20121119062707_add_discounted_pricing.rb
@@ -1,6 +1,6 @@
 class AddDiscountedPricing < ActiveRecord::Migration
   def self.up
-    add_column :courses, :discounted_pricing, :string, :default => nil
+    add_column :courses, :discounted_pricing, :text, :default => nil
   end
 
   def self.down
