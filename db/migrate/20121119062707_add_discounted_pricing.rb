50147f67ef82a44dfcc89c419223787ac40ab560
Sid Viswanathan <sid@cardmunch.com>
2012-11-20 11:29:15 -0800

    updated the migration to not have a default value

--- db/migrate/20121119062707_add_discounted_pricing.rb
+++ db/migrate/20121119062707_add_discounted_pricing.rb
@@ -1,6 +1,6 @@
 class AddDiscountedPricing < ActiveRecord::Migration
   def self.up
-    add_column :courses, :discounted_pricing, :text, :default => nil
+    add_column :courses, :discounted_pricing, :text
   end
 
   def self.down
