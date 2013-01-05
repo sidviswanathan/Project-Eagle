4e0f61cd74a21032e0996cdefa7cbc8afaf5836f
Sid Viswanathan <sid@cardmunch.com>
2013-01-05 13:59:36 -0800

    added code to environment.rb for rvm dep issue

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,18 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+#Fixes some gem dependency issue for RVM
+if Gem::VERSION >= "1.3.6"
+ module Rails
+   class GemDependency
+     def requirement
+       r = super
+       (r == Gem::Requirement.default) ? nil : r
+     end
+   end
+ end
+end
+
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
